7c2c8e9a813d1ec235479479e4136c846656afc7
Felix Du <felix.du@nokia-sbell.com>
2025-04-21 12:42:39 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.diagnostic.enable(false)
+vim.diagnostic.enable(false) -- <leader>ud	Toggle Diagnostics
 vim.opt.relativenumber = false
 vim.opt.conceallevel = 0

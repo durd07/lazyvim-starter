4a11959d8f2dbcdb483bcb5d41172cb6f5139626
Felix Du <felix.du@nokia-sbell.com>
2025-10-09 15:26:44 +0800

    update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,7 +1,7 @@
 return {
   -- https://github.com/LazyVim/LazyVim/issues/6039
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   -- {
   --   "HakonHarnes/img-clip.nvim",
   --   event = "VeryLazy",
